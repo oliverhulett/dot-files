99b1e389e5fe2f1337c1748e6ab1b3496fbc4f76
Oliver Hulett <Oliver.Hulett@Optiver.com.au>
2015-09-03 00:00:02 +1000

    Autocommit: Thu, 03 Sep 2015 00:00:01 +1000

--- bash_aliases/links.sh
+++ bash_aliases/links.sh
@@ -11,3 +11,17 @@ function rmbrokenlinks
 	find -L "$@" -not \( -name '.git' -prune -or -name '.svn' -prune \) -type l -exec ls -hdl --color=always "{}" \; -delete -print
 }
 
+unalias replacelink 2>/dev/null
+function replacelink
+{
+	link="${2%%/}"
+	if [ ! -h "$link" ]; then
+		echo "$link is not a link"
+		return 1
+	fi
+	orig="$(readlink -f "$link")"
+	rm -v "$link" && ln -sv "$1" "$link" && return 0
+	echo "Failed to replace link $link with '$1'"
+	echo "Restoring original link to '$orig'"
+	ln -sv "$orig" "$link"
+}
