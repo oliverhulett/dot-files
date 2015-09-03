3bf4422ebe2362742e88666e32a1c396f8e1f68e
Oliver Hulett <Oliver.Hulett@Optiver.com.au>
2015-09-04 00:00:01 +1000

    Autocommit: Fri, 04 Sep 2015 00:00:01 +1000

--- git_wrappers/commit.sh
+++ git_wrappers/commit.sh
@@ -7,12 +7,14 @@ echo
 git status
 
 branch="$(git branch --no-color | sed -nre 's/^\* //p' | cut -d'_' -f1)"
+startmode="-c startinsert"
 if [ "$branch" == "master" ]; then
 	branch=
 	read -p "Prepend a ticket to commit message? [Y/n/q] " -n1 -r
 	echo
 	if [ "`echo $REPLY | tr [A-Z] [a-z]`" == "n" ]; then
 		branch=
+		startmode=
 	elif [ "`echo $REPLY | tr [A-Z] [a-z]`" == "q" ]; then
 		exit
 	else
@@ -25,6 +27,7 @@ else
 	echo
 	if [ "`echo $REPLY | tr [A-Z] [a-z]`" == "n" ]; then
 		branch=
+		startmode=
 	elif [ "`echo $REPLY | tr [A-Z] [a-z]`" == "o" ]; then
 		echo -n "$(tput bold)Ticket:$(tput sgr0)  "
 		read
@@ -36,7 +39,11 @@ else
 	fi
 fi
 
+if grep -qE '^[^#].*$' .git/COMMIT_EDITMSG 2>/dev/null >/dev/null; then
+	startmode=
+fi
+
 sed -re '/^#/! s/^('"${branch}"')?(.+)/'"${branch}"'\2/' .git/COMMIT_EDITMSG -i
 
-vim -c "autocmd InsertLeave <buffer> let [c, l] = [getpos('.'), strlen(getline('.'))]" -c "autocmd InsertLeave <buffer> 1,!sed -re 's/^(${branch})?(.+)/${branch}\2/'" -c "autocmd InsertLeave <buffer> call setpos('.', c) | if l < strlen(getline('.')) | call setpos('.', [c[0], c[1], c[2] + ${#branch}, c[3]])" "$@"
+vim -c "autocmd InsertLeave <buffer> let [c, l] = [getpos('.'), strlen(getline('.'))]" -c "autocmd InsertLeave <buffer> 1,!sed -re 's/^(${branch})?(.+)/${branch}\2/'" -c "autocmd InsertLeave <buffer> call setpos('.', c) | if l < strlen(getline('.')) | call setpos('.', [c[0], c[1], c[2] + ${#branch}, c[3]])" $startmode "$@"
 
