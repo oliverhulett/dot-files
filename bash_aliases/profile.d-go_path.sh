2d539ea3875783a4736a291add8ffdb8289e0a4d
Oliver Hulett <Oliver.Hulett@Optiver.com.au>
2016-01-16 00:00:03 +1100

    Autocommit: Sat, 16 Jan 2016 00:00:01 +1100

--- bash_aliases/profile.d-go_path.sh
+++ bash_aliases/profile.d-go_path.sh
@@ -3,5 +3,5 @@ export GOTOOLS="/home/olihul/3rd_party_tools/gotools"
 export GO_VER=
 
 # We want a slightly more subtle equivalent of PATH=$PATH:$GOROOT/bin:$GOTOOLS/bin
-PATH="$(echo "$PATH" | sed -re "s!(^|:)$GOROOT/bin/?(:|$)!\1!;s!(^|:)$GOTOOLS/bin/?(:|$)!\1!"):$GOROOT/bin:$GOTOOLS/bin"
+PATH="$GOROOT/bin:$GOTOOLS/bin:$(echo "$PATH" | sed -re "s!(^|:)$GOROOT/bin/?(:|$)!\1!;s!(^|:)$GOTOOLS/bin/?(:|$)!\1!")"
 export PATH
