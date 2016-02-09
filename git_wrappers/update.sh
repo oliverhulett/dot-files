b5da61768f14d5a336e7e11b5a25f569eafc96c4
Oliver Hulett <Oliver.Hulett@Optiver.com.au>
2016-02-10 00:00:02 +1100

    Autocommit: Wed, 10 Feb 2016 00:00:02 +1100

--- git_wrappers/update.sh
+++ git_wrappers/update.sh
@@ -6,6 +6,10 @@ if [ "$1" == "-c" -o "$1" == "--clean" ]; then
 	if [ -f ./pins.json ]; then
 		sed -nre 's/^[ \t]+"(.+)": \{/\1/p' ./pins.json | tee >(xargs rm -rf)
 	fi
+	find ./ -not \( -name .git -prune -or -name .svn -prune \) -name externals.json | while read; do
+		echo "Removing externals from: $REPLY"
+		sed -nre 's/^[ \t]+"(.+)": \{/\1/p' "$REPLY" | tee >(xargs rm -rf)
+	done
 fi
 
 if [ -x ./git_setup.py ]; then
