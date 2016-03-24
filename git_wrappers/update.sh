4fcce03594c28b4af2a33cb645b3418cdcfa6317
Oliver Hulett <Oliver.Hulett@Optiver.com.au>
2016-03-25 00:00:02 +1100

    Autocommit: Fri, 25 Mar 2016 00:00:01 +1100

--- git_wrappers/update.sh
+++ git_wrappers/update.sh
@@ -3,11 +3,12 @@
 ## `git updat e-c` is a common typo when fingers outpace brains.  Git will correctly guess that you meant `git update` but not that you meant `git update -c`.
 if [ "$1" == "-c" -o "$1" == "--clean" -o "$1" == "e-c" ]; then
 	if [ -f ./pins.json ]; then
+		echo "Removing externals from: pins.json"
 		sed -nre 's/^[ \t]+"(.+)": \{/\1/p' ./pins.json | tee >(xargs rm -rf)
 	fi
 	find ./ -not \( -name .git -prune -or -name .svn -prune \) -name externals.json | while read; do
 		echo "Removing externals from: $REPLY"
-		sed -nre 's/^[ \t]+"(.+)": \{/\1/p' "$REPLY" | tee >(xargs rm -rf)
+		sed -nre 's/^[ \t]+"(.+)": \{/\1/p' "$REPLY" | tee >(xargs rm -vrf)
 	done
 	echo "Removing '.git/externals/' and likely external directories: " x_*
 	rm -rf .git/externals x_* 2>/dev/null
