d14910058e9068a466ce15a5e57240882677018c
Oliver Hulett <Oliver.Hulett@Optiver.com.au>
2016-04-06 00:00:02 +1000

    Autocommit: Wed, 06 Apr 2016 00:00:01 +1000

--- git_wrappers/update.sh
+++ git_wrappers/update.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
+## Until v2.5 git would pre-pend /usr/bin to path, which means the wrong python is found.
+source "$(dirname "$(readlink -f "$0")")/../bash_aliases/profile.d-pyvenv.sh"
+## If we're using externals.json, check that it is sane.
+if [ -f ./externals.json ]; then
+	python -m json.tool ./externals.json
+elif [ -f ./deps.json ]; then
+	python -m json.tool ./deps.json
+fi
+
 ## `git updat e-c` is a common typo when fingers outpace brains.  Git will correctly guess that you meant `git update` but not that you meant `git update -c`.
 if [ "$1" == "-c" -o "$1" == "--clean" -o "$1" == "e-c" ]; then
 	if [ -f ./pins.json ]; then
 		echo "Removing externals from: pins.json"
-		sed -nre 's/^[ \t]+"(.+)": \{/\1/p' ./pins.json | tee >(xargs rm -rf)
+		sed -nre 's/^[ \t]+"(.+)": \{/\1/p' ./pins.json | tee >(xargs rm -rf) | xargs
 	fi
 	find ./ -not \( -name .git -prune -or -name .svn -prune \) -name externals.json | while read; do
 		echo "Removing externals from: $REPLY"
-		sed -nre 's/^[ \t]+"(.+)": \{/\1/p' "$REPLY" | tee >(xargs rm -rf)
+		sed -nre 's/^[ \t]+"(.+)": \{/\1/p' "$REPLY" | tee >(xargs rm -rf) | xargs
 	done
 	echo "Removing '.git/externals/' and likely external directories: " x_*
 	rm -rf .git/externals x_* 2>/dev/null
