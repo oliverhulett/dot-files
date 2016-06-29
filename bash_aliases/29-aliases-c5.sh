88f3606707b93033e4b72943fc54c655aa8bdddf
Oliver Hulett <Oliver.Hulett@Optiver.com.au>
2016-06-29 13:47:41 +1000

    Wrap c5 command so that it loads bashrc first and gets the environment right

---
+++ bash_aliases/29-aliases-c5.sh
@@ -0,0 +1,26 @@
+# docker container to build things under c5
+function c5()
+{
+	(
+		TMP="$(mktemp -t "docker.$(basename "$1").XXXXXXXXXX")"
+		real_cat >"$TMP" <<-'EOF'
+			#!/bin/bash
+			## LESSOPEN does not work on c5 :(
+			LESSOPEN=
+			source ~/.bashrc
+			"$@"
+		EOF
+		chmod u+x "$TMP"
+
+		docker run -u `id -u` -h `hostname` \
+			-v /etc:/etc -v ~/:`echo $HOME`/ -v `pwd`:/src -w /src --env-file=<(/usr/bin/env) \
+			-v "$TMP":"$TMP" --entrypoint="$TMP" \
+			--tty=true --interactive=true \
+			docker-registry.aus.optiver.com/servicedelivery/el5-development "$@"
+
+		rm -fv "$TMP"
+	)
+}
+
+alias c5build.py='c5 ./build.py --output-dir=build_c5'
+
