2acb6c80cb01ac344a14c3332a0bde5aca68d945
Oliver Hulett <Oliver.Hulett@Optiver.com.au>
2016-06-29 12:20:19 +1000

    Python26 venv for docker images.

--- bash_aliases/profile.d-pyvenv.sh
+++ bash_aliases/profile.d-pyvenv.sh
@@ -1,6 +1,6 @@
 # Guard against re-entrance!
-if [ "${PYVENV_GUARD}" != "__ENTERED_PYVENV__$(cat ${BASH_SOURCE} 2>/dev/null | md5sum)" ]; then
-        PYVENV_GUARD="__ENTERED_PYVENV__$(cat ${BASH_SOURCE} 2>/dev/null | md5sum)"
+if [ "${PYVENV_GUARD}" != "__ENTERED_PYVENV__$( ( echo ${PYVENV_HOME} && cat ${BASH_SOURCE} 2>/dev/null ) | md5sum)" ]; then
+	PYVENV_GUARD="__ENTERED_PYVENV__$( ( echo ${PYVENV_HOME} && cat ${BASH_SOURCE} 2>/dev/null ) | md5sum)"
 else
 	return
 fi
@@ -8,6 +8,9 @@ fi
 source "/home/olihul/etc/dot-files/bash_common.sh"
 
 export PYVENV_HOME="/home/olihul/pyvenv"
+if [ -f /.dockerenv ]; then
+	export PYVENV_HOME="/home/olihul/py26venv"
+fi
 
 /home/olihul/bin/virtualenv --no-site-packages "$PYVENV_HOME" >/dev/null 2>/dev/null
 VIRTUAL_ENV_DISABLE_PROMPT=1 source "$PYVENV_HOME/bin/activate"
