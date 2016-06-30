dcbf43988c078b22b7bae98d726e69e98d98525d
Oliver Hulett <Oliver.Hulett@Optiver.com.au>
2016-06-30 17:04:20 +1000

    Unpin externals that aren't pointed at a tag

---
+++ git_wrappers/unpin_externals.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+## Until v2.5 git would pre-pend /usr/bin to path, which means the wrong python is found.
+source "$(dirname "$(readlink -f "$0")")/../bash_aliases/09-profile.d-pyvenv.sh"
+if [ ! -f ./externals.json ]; then
+	echo "Cannot modify externals.  No externals.json found."
+	exit 1
+fi
+
+python <<EOF
+import os
+import json
+import subprocess
+
+def do_file(name):
+    global files
+    with open(name) as f:
+        xternals = json.load(f)
+    if '@import' in xternals:
+        print xternals['@import']
+        files += [os.path.join(n, 'externals.json') for n in xternals['@import']]
+    for key in xternals.iterkeys():
+        if 'ref' in xternals[key]:
+            p = subprocess.Popen(['git', 'rev-parse', '-q', '--verify', 'refs/tags/{0}'.format(xternals[key]['ref'])], stdout=subprocess.PIPE, cwd=key)
+            p.communicate()
+            if p.returncode != 0:
+                print "Un-pinning non-tag: {0} @ {1}".format(key, xternals[key]['ref'])
+                del xternals[key]['rev']
+    with open(name, 'w') as f:
+        json.dump(xternals, f, indent=4)
+
+files = ['externals.json']
+while len(files) > 0:
+    print files
+    do_file(files.pop(0))
+EOF
+python -m json.tool "externals.json" > /dev/null && echo "$(python -m json.tool "externals.json")" > "externals.json"
+
+echo
+if [ -x ./git_setup.py ]; then
+	./git_setup.py -kq
+else
+	getdep
+fi
+
