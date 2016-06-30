e53d2e0a6b2e9bd51f95bfeb8ee4e26cba9d3bfc
Oliver Hulett <Oliver.Hulett@Optiver.com.au>
2016-06-30 17:04:56 +1000

    After updating, only pin tags, leave non-tags unpinned

--- git_wrappers/update_externals.sh
+++ git_wrappers/update_externals.sh
@@ -11,6 +11,9 @@ if [ ! -f ./externals.json ]; then
 	exit 1
 fi
 
+## TODO:  Remove this when they've all been updated...
+find . -xdev -not \( -name '.git' -prune -or -name '.svn' -prune \) -name 'externals.json' | while read; do sed -re 's/git@git:7999/git@git.comp.optiver.com:7999/' "$REPLY" -i; done
+
 git pull --all
 if [ -x ./git_setup.py ]; then
 	./git_setup.py -kq
@@ -52,5 +55,33 @@ else
 fi
 
 ./.git/git_utils/git_utils/pin_externals.py
+echo
+python <<EOF
+import os
+import json
+import subprocess
+
+def do_file(name):
+    global files
+    with open(name) as f:
+        xternals = json.load(f)
+    if '@import' in xternals:
+        print xternals['@import']
+        files += [os.path.join(n, 'externals.json') for n in xternals['@import']]
+    for key in xternals.iterkeys():
+        if 'ref' in xternals[key]:
+            p = subprocess.Popen(['git', 'rev-parse', '-q', '--verify', 'refs/tags/{0}'.format(xternals[key]['ref'])], stdout=subprocess.PIPE, cwd=key)
+            p.communicate()
+            if p.returncode != 0:
+                print "Un-pinning non-tag: {0} @ {1}".format(key, xternals[key]['ref'])
+                del xternals[key]['rev']
+    with open(name, 'w') as f:
+        json.dump(xternals, f, indent=4)
+
+files = ['externals.json']
+while len(files) > 0:
+    print files
+    do_file(files.pop(0))
+EOF
 python -m json.tool "externals.json" > /dev/null && echo "$(python -m json.tool "externals.json")" > "externals.json"
 
