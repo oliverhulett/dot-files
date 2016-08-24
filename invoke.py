e31b6fa23cc93303ec892e96bcbd1a81af2c7a33
Oliver Hulett <oliver.hulett@optiver.com.au>
2016-08-25 00:10:01 +1000

    Autocommit: Thu, 25 Aug 2016 00:10:01 +1000

--- invoke.py
+++ invoke.py
@@ -13,6 +13,8 @@ _environ['SMB_USER'] = 'olihul'
 _environ['SMB_PASS'] = '4tyTw0!?'
 _environ['WIKI_USER'] = _auth[0]
 _environ['WIKI_PASS'] = _auth[1]
+_environ['JIRA_USER'] = _auth[0]
+_environ['JIRA_PASS'] = _auth[1]
 
 ## Special case for my crazy bashrc-ness, doesn't play nicely with invoke's case normalisation
 _environ.pop('http_proxy', None)
