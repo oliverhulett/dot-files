4344b4114c16b11d795c4e435ec758bbb30130e0
Oliver Hulett <oliver.hulett@optiver.com.au>
2016-09-07 00:10:01 +1000

    Autocommit: Wed, 07 Sep 2016 00:10:01 +1000

--- bash_aliases/29-aliases-sort_unique.sh
+++ bash_aliases/29-aliases-sort_unique.sh
@@ -1,23 +1,55 @@
 ## Call sort on files, write results back to files
 function sortinline()
 {
+	KEEP_BLANK="no"
 	ARGS=()
 	FILES=()
 	for f in "$@"; do
 		if [ -f "$f" ]; then
 			FILES[${#FILES[@]}]="$f"
 		else
+			if [ "$f" == "--keep" ]; then
+				KEEP_BLANK="yes"
+			fi
 			ARGS[${#ARGS[@]}]="$f"
 		fi
 	done
 	for f in "${FILES[@]}"; do
 		echo "sort ${ARGS[@]} $f"
 		echo "$(sort "${ARGS[@]}" "$f")" >"$f"
+		if [ "$KEEP_BLANK" == "no" ]; then
+			cleaninline $f
+		fi
 	done
 }
 
 ## Call uniq on files, write results back to files
 function uniqinline()
+{
+	KEEP_BLANK="no"
+	ARGS=()
+	FILES=()
+	for f in "$@"; do
+		if [ -f "$f" ]; then
+			FILES[${#FILES[@]}]="$f"
+		else
+			if [ "$f" == "--keep" ]; then
+				KEEP_BLANK="yes"
+			fi
+			ARGS[${#ARGS[@]}]="$f"
+		fi
+	done
+	for f in "${FILES[@]}"; do
+		echo "uniq ${ARGS[@]} $f"
+		echo "$(uniq "${ARGS[@]}" "$f")" >"$f"
+		if [ "$KEEP_BLANK" == "no" ]; then
+			cleaninline $f
+		fi
+	done
+}
+
+## Clean files of empty lines, write result back to files
+function cleaninline()
 {
 	ARGS=()
 	FILES=()
@@ -29,8 +61,8 @@ function uniqinline()
 		fi
 	done
 	for f in "${FILES[@]}"; do
-		echo "uniq ${ARGS[@]} $f"
-		echo "$(uniq "${ARGS[@]}" "$f")" >"$f"
+		echo "sed -re '/^$/d' ${ARGS[@]} -i $f"
+		sed -re '/^$/d' "${ARGS[@]}" -i "$f"
 	done
 }
 
