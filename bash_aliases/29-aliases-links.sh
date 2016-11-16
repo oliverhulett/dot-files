a87a9b63c27c13911d86fd2f2e30ac37f686c887
Oliver Hulett <oliver.hulett@optiver.com.au>
2016-11-17 00:10:02 +1100

    Autocommit: Thu, 17 Nov 2016 00:10:01 +1100

--- bash_aliases/29-aliases-links.sh
+++ bash_aliases/29-aliases-links.sh
@@ -15,13 +15,17 @@ unalias replacelink 2>/dev/null
 function replacelink
 {
 	link="${2%%/}"
-	if [ ! -h "$link" ]; then
-		echo "$link is not a link"
-		return 1
+	if [ -h "$link" ]; then
+		orig="$(readlink -f "$link")"
+		rm -v "$link" && ln -sv "$1" "$link" && return 0
+		echo "Failed to replace link $link with '$1'"
+		echo "Restoring original link to '$orig'"
+		ln -sv "$orig" "$link"
+	else
+		mv "$link" "${link}.bak"
+		ln -sv "$1" "$link" && return 0
+		echo "Failed to replace $link with a link to '$1'"
+		echo "Restoring original"
+		mv "${link}.bak" "$link"
 	fi
-	orig="$(readlink -f "$link")"
-	rm -v "$link" && ln -sv "$1" "$link" && return 0
-	echo "Failed to replace link $link with '$1'"
-	echo "Restoring original link to '$orig'"
-	ln -sv "$orig" "$link"
 }
