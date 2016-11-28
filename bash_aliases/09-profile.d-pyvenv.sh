50892e0759d3cb0ae0dc8c84dbf71fb98aecc889
Oliver Hulett <oliver.hulett@optiver.com.au>
2016-11-29 00:10:01 +1100

    Autocommit: Tue, 29 Nov 2016 00:10:01 +1100

--- bash_aliases/09-profile.d-pyvenv.sh
+++ bash_aliases/09-profile.d-pyvenv.sh
@@ -14,7 +14,7 @@ PYVENV_MARKER="${PYVENV_HOME}/.mark"
 if [ ! -e "${PYVENV_MARKER}" ] || [ "$("$REAL_CAT" "${PYVENV_MARKER}" 2>/dev/null)" != "$(pyvenv_version)" ]; then
 	## Switch the ${HOME}/bin/virtualenv symlink to use a different version
 	${HOME}/bin/virtualenv --no-site-packages -p /usr/bin/python2.7 "$PYVENV_HOME" >/dev/null 2>/dev/null
-	( cd ${PYVENV_HOME}/bin && ln -s python python26 )
+	( cd ${PYVENV_HOME}/bin && ln -s python python26 2>/dev/null )
 	VIRTUAL_ENV_DISABLE_PROMPT=1 source "$PYVENV_HOME/bin/activate"
 
 	pyvenv_version >"${PYVENV_MARKER}"
