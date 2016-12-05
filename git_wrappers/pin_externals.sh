9a58475178b0220a256d3692ad6d0bf361989a74
Oliver Hulett <oliver.hulett@optiver.com.au>
2016-12-06 00:10:01 +1100

    Autocommit: Tue, 06 Dec 2016 00:10:01 +1100

--- git_wrappers/pin_externals.sh
+++ git_wrappers/pin_externals.sh
@@ -2,30 +2,16 @@
 
 ## Until v2.5 git would pre-pend /usr/bin to path, which means the wrong python is found.
 source "$(dirname "$(readlink -f "$0")")/../bash_aliases/09-profile.d-pyvenv.sh" || true
-if [ ! -x .git/git_utils/git_utils/pin_externals.py ]; then
-	echo "Cannot pin externals.  git_utils is not installed."
-	exit 1
-fi
-if [ ! -f ./externals.json ]; then
-	echo "Cannot pin externals.  No externals.json found."
-	exit 1
-fi
 
-if [ -x ./git_setup.py ]; then
-	python ./git_setup.py -kq
-else
-	getdep
-fi
+getdep -s
 
 echo
 echo "Pinning externals"
-python ./.git/git_utils/git_utils/pin_externals.py
-python -m json.tool "externals.json" > /dev/null && echo "$(python -m json.tool "externals.json")" > "externals.json"
+for d in $(find . -xdev -not \( -name '.git' -prune -or -name '.svn' -prune \) -name 'externals.json'); do
+	( cd $(dirname $d) && python -m getdep.pin_externals )
+	( cd $(dirname $d) && python -m json.tool "externals.json" > /dev/null && echo "$(python -m json.tool "externals.json")" > "externals.json" )
+done
 echo
 
-if [ -x ./git_setup.py ]; then
-	python ./git_setup.py -kq
-else
-	getdep
-fi
+getdep -s
 
