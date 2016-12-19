3cd30aeb10f7f7931cb1cd9f5d5d5d0d7cedeba3
Oliver Hulett <oliver.hulett@optiver.com.au>
2016-12-20 00:10:01 +1100

    Autocommit: Tue, 20 Dec 2016 00:10:01 +1100\n# On branch master # Changes not staged for commit: #   (use "git add <file>..." to update what will be committed) #   (use "git checkout -- <file>..." to discard changes in working directory) # #	modified:   bin/docker-run.sh #	modified:   crontab # no changes added to commit (use "git add" and/or "git commit -a")

--- bin/docker-run.sh
+++ bin/docker-run.sh
@@ -9,19 +9,22 @@ while ! echo "$IMAGES" | grep -qw "$1" 2>/dev/null >/dev/null; do
 	DOCKER_RUN_ARGS[${#DOCKER_RUN_ARGS}]="$1"
 	shift
 done
-NAME="$1"
+IMAGE="$1"
 shift
 if [ $# == 0 ]; then
 	set -- /bin/bash
 fi
 
-LABELS="$(docker inspect $NAME | jq '.[0].Config.Labels')"
+# user specific container name
+NAME=`basename $IMAGE`-`whoami`-`date "+%s"`
+echo "Starting $NAME ($IMAGE)"
+LABELS="$(docker inspect $IMAGE | jq '.[0].Config.Labels')"
 # Use a docker container to do things
 TMP="$(mktemp -t "docker.$(basename "$1").XXXXXXXXXX")"
-trap 'echo "Leaving ${NAME}" && rm -fv ${TMP}' EXIT
+trap 'echo "Leaving $NAME (${IMAGE})" && rm -fv ${TMP}' EXIT
 "$REAL_CAT" >"$TMP" <<-EOF
 	#!/bin/bash
-	export PROMPT_PREFIX="(docker:$(basename $NAME)) "
+	export PROMPT_PREFIX="(docker:$(basename $IMAGE)) "
 	source ~/.bashrc
 	"\$@"
 EOF
@@ -31,14 +34,16 @@ TTY=
 if [ -t 1 ]; then
 	TTY="--tty=true --interactive=true"
 fi
+# Check if ssh agents are being used
+[ -z $SSH_AUTH_SOCK ] && SSH_PASS_THROUGH="" || SSH_PASS_THROUGH="-v $SSH_AUTH_SOCK:/ssh-agent -e SSH_AUTH_SOCK=/ssh-agent"
 for cmd in echo ""; do
-	$cmd docker run -u `id -u` -h `hostname` --cpu-shares=`nproc` \
+	$cmd docker run -u `id -u` -h `hostname` --cpu-shares=`nproc` --privileged --name=${NAME} \
 		-v /etc/passwd:/etc/passwd -v /etc/shadow:/etc/shadow -v /etc/group:/etc/group -v /etc/gshadow:/etc/gshadow \
 		-v /etc/sudo.conf:/etc/sudo.conf -v /etc/sudoers:/etc/sudoers -v /etc/sudoers.d:/etc/sudoers.d -v /etc/pam.d:/etc/pam.d \
-		-v /var/run/docker.sock:/var/run/docker.sock \
+		-v /var/run/docker.sock:/var/run/docker.sock $SSH_PASS_THROUGH \
 		-v "${HOME}":"${HOME}" -v "`pwd`":"`pwd`" -w "`pwd`" --env-file=<(/usr/bin/env) \
 		-v "$TMP":"$TMP" --entrypoint="$TMP" \
 		--rm ${TTY} "${DOCKER_RUN_ARGS[@]}" \
-		$NAME "$@"
+		$IMAGE "$@"
 done
 
