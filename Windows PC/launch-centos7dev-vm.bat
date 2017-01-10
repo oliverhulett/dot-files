d196554be36aa4e17c18d162f05b8257bdb3478b
Oliver Hulett <oliver.hulett@optiver.com.au>
2017-01-10 13:44:58 +1100

    Vagrant files autocommit: Tue, 10 Jan 2017 13:44:58 +1100 # On branch master # Changes to be committed: #   (use "git reset HEAD <file>..." to unstage) # #	new file:   Windows PC/Vagrantfile #	new file:   Windows PC/launch-centos7dev-vm.bat #	new file:   Windows PC/setup.sh #	new file:   Windows PC/vbox_manage.rb # # Changes not staged for commit: #   (use "git add <file>..." to update what will be committed) #   (use "git checkout -- <file>..." to discard changes in working directory) # #	modified:   Windows PC/Vagrantfile #

---
+++ Windows PC/launch-centos7dev-vm.bat
@@ -0,0 +1,8 @@
+cd /D C:\Repo\VirtualBox VMs
+"C:\Program Files\Cmder\cmder.exe" /start "C:\Repo\VirtualBox VMs"
+REM vagrant halt
+REM vagrant destroy -f
+REM vagrant box update
+REM vagrant up
+REM pause
+REM vagrant halt
---
+++ Windows PC/vbox_manage.rb
@@ -0,0 +1,48 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+VM_NAME ||= ENV['VM_NAME'] || "centos_dev_vm"
+
+def vbox_manage?
+  @vbox_manage ||= ! `which VBoxManage`.chomp.empty?
+end
+
+def vm_boxes
+  boxes = {}
+  if vbox_manage?
+    vms = `VBoxManage list vms`
+    vms.split("\n").each do |vm|
+      x = vm.split
+      k = x[0].gsub('"','')      # vm name
+      v = x[1].gsub(/[{}]/,'')   # vm UUID
+      boxes[k] = v
+    end
+  end
+  boxes
+end
+
+def vm_exists?
+  vm_boxes[VM_NAME] ? true : false
+end
+
+def vm_info
+  vm_exists? ? `VBoxManage showvminfo #{VM_NAME}` : ''
+end
+
+def vm_uuid
+  vm_boxes[VM_NAME]
+end
+
+def vm_state
+  case vm_exists?
+  when true
+    vm_state = vm_info.split("\n").select {|f| f =~ /^State:/}.first || ''
+    vm_state.split[1]
+  when false
+    ''
+  end
+end
+
+def vm_running?
+  vm_state == "running"
+end
