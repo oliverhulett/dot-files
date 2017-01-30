9f3ca79a3e21e639801c3315c6cf266eca786412
Oliver Hulett <oliver.hulett@optiver.com.au>
2017-01-30 14:16:55 +1100

    Backtesting env vars

--- Windoze/bin/path_setup.bat
+++ Windoze/bin/path_setup.bat
@@ -4,3 +4,6 @@ set path=%PATH%;C:\HashiCorp\Vagrant\bin
 set path=%PATH%;C:\Program Files\Oracle\VirtualBox
 set path=%PATH%;C:\Program Files (x86)\Anaconda2;C:\Program Files (x86)\Anaconda2\Scripts;C:\Program Files (x86)\Anaconda2\Library\bin;C:\Program Files (x86)\Anaconda2\libs
 set path=%PATH%;Z:\CommonApps\Onetick\client\current\one_market_data\one_tick\bin;Z:\CommonApps\Onetick\client\current\one_market_data\one_tick\lib;
+
+set BACKTESTS=D:\backtests
+set DIFFTOOL="gvimdiff -f"
