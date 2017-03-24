5f0e1fd3f2eb79712204c8b70fd9036257f7ba60
Oliver Hulett <oliver.hulett@optiver.com.au>
2017-03-24 13:53:26 +1100

    print what you're doing

--- Windoze/vagrant/restart.bat
+++ Windoze/vagrant/restart.bat
@@ -1,11 +1,27 @@
 HERE=%~d0
 cd /d %HERE%
 
+echo "STOPPING VM"
+echo
 vagrant halt
+echo
+echo "DESTROYING VM"
+echo 
 vagrant destroy -f
+echo
+echo "MAILING PATCHES HOME"
+echo
 ..\sync2home\mail_patches_home.sh
+echo
+echo "UPDATING BASE IMAGE"
+echo
 vagrant box update
+echo
+echo "STARTING VM"
+echo
 vagrant up
+echo
+echo "DONE"
 
 REM pause
 REM vagrant halt
