33423ade1f7dd41f4cbf8398f91a3d2a9f630045
Oliver Hulett <oliver.hulett@optiver.com.au>
2017-04-12 12:22:41 +1000

    Fixes for new HDD

--- Windoze/vagrant/setup.sh
+++ Windoze/vagrant/setup.sh
@@ -18,7 +18,7 @@ echo "Setting up sudo..."
 echo 'Defaults    secure_path = /home/olihul/dot-files/bin:/home/olihul/bin:/home/olihul/sbin:/optiver/bin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin' >/etc/sudoers.d/99_olihul
 
 echo "Restoring backups..."
-mkdir ${HOME}/etc
+mkdir ${HOME}/etc 2>/dev/null
 for f in passwd release.auth; do
 	cp -v /H_DRIVE/etc/$f ${HOME}/etc/$f
 	chown ${USER}:users ${HOME}/etc/$f
@@ -30,34 +30,45 @@ if [ -e "${HOME}/etc/passwd" ]; then
 	( cat ${HOME}/etc/passwd; cat ${HOME}/etc/passwd; ) | smbpasswd -sa ${USER}
 
 	echo "Bootstrapping GIT SSH keys..."
-	curl -u "${USER}:$(cat ${HOME}/etc/passwd)" -X POST -H "Accept: application/json" -H "Content-Type: application/json" https://git/rest/ssh/1.0/keys -d '{"text": "'"$(cat ${HOME}/.ssh/id_rsa.pub)"'"}'
+	curl -u "${USER}:$(cat ${HOME}/etc/passwd)" -X POST -H "Accept: application/json" -H "Content-Type: application/json" https://git/rest/ssh/1.0/keys -d '{"text": "'"$(cat ${HOME}/.ssh/id_rsa.pub)"'"}' 2>/dev/null
 fi
 
 echo "Cloning dot-files..."
-su -c "git clone --recursive ssh://git@git.comp.optiver.com:7999/~${USER}/dot-files.git ${HOME}/dot-files" ${USER}
+su -c "cd ${HOME} && git config --global user.name 'Oliver Hulett' && git config --global user.email oliver.hulett@optiver.com.au" ${USER}
+su -c "cd ${HOME} && git clone ssh://git@git.comp.optiver.com:7999/~${USER}/dot-files.git ${HOME}/dot-files" ${USER}
+source "${HOME}/dot-files/bash_aliases/19-env-proxy.sh"
+proxy_setup -q ${USER}
+su -c "cd ${HOME}/dot-files && git submodule update" ${USER}
 su -c "cd ${HOME}/dot-files && git commit --allow-empty -aqm "'"Vagrant setup autocommit: $(date -R)\n$(git status --short)"'" && git pull" ${USER}
 
+mkdir ${HOME}/.dotlogs 2>/dev/null
+chown ${USER}:users ${HOME}/.dotlogs
 source "${HOME}/dot-files/bash_common.sh" && eval "${capture_output}" || true
 export PATH="$(prepend_path "${HOME}/dot-files/bin")"
 
-if ! echo "${HTTP_PROXY}" | grep -q "${USER}"; then
-	source "${HOME}/dot-files/bash_aliases/19-env-proxy.sh"
-	proxy_setup -q ${USER}
-fi
 su -c "${HOME}/dot-files/setup-home.sh" ${USER}
 
 systemctl link "${HOME}/dot-files/autocommit.service"
 systemctl start autocommit.service
 
 echo "General clean-ups..."
-rm -rf ${HOME}/Desktop
-rmdir ${HOME}/{Documents,Downloads,Music,Pictures,Public,Templates,Videos}
+rm -rf ${HOME}/Desktop 2>/dev/null
+rmdir ${HOME}/{Documents,Downloads,Music,Pictures,Public,Templates,Videos} 2>/dev/null
 systemctl stop collectd.service
 systemctl disable collectd.service
 systemctl stop timekeeper.service
 systemctl disable timekeeper.service
 sed --in-place -re 's/^CRONDARGS=(.*)-m ?off(.*)/CRONDARGS=\1\2/' /etc/sysconfig/crond
 systemctl restart crond.service
+
+echo "Restoring KDE and other personal configs..."
+rsync -rAXog /H_DRIVE/etc ${HOME}/
+find ${HOME}/etc -type f -print0 | xargs -0 chmod -x
+chown -R ${USER}:users ${HOME}/etc
+rsync -rAXog --update ${HOME}/etc/backups/${HOME}/ ${HOME}/
+		
+echo "Restarting KDE to pick up restored backups..."
+sudo systemctl restart gdm.service
 
 echo "Installing some things I don't want to docker all the time..."
 (
@@ -99,6 +110,16 @@ echo "Installing some things I don't want to docker all the time..."
 disown -h
 disown
 
+echo "Copying cc-env custom files for eclipse indexer and friends..."
+(
+	CC_EXE="/usr/local/bin/cc-env"
+	CC_IMAGE="$(sed -nre 's!.+(docker-registry\.aus\.optiver\.com/[^ ]+/[^ ]+).*!\1!p' "${CC_EXE}" | tail -n1)"
+	mkdir --parents /media/cc-env/opt/ || true
+	${HOME}/dot-files/bin/docker-run.sh -v /media/cc-env:/media/cc-env -u 0 ${CC_IMAGE} rsync -vpPAXrogthlm --delete /opt/optiver/ /media/cc-env/opt/optiver/
+) >&${log_fd} 2>&${log_fd} &
+disown -h
+disown
+
 echo "Restoring local installs and other backups..."
 (
 	for d in /H_DRIVE/*; do
@@ -108,16 +129,6 @@ echo "Restoring local installs and other backups..."
 		chown -R ${USER}:users ${HOME}/$d
 	done
 	chmod +x ${HOME}/opt/pyvenv/bin/* ${HOME}/opt/eclipse/eclipse ${HOME}/opt/sublime_text_3/sublime_text ${HOME}/opt/subl.sh ${HOME}/opt/clion-2016.3.2/bin/clion.sh
-) >&${log_fd} 2>&${log_fd} &
-disown -h
-disown
-
-echo "Copying cc-env custom files for eclipse indexer and friends..."
-(
-	CC_EXE="/usr/local/bin/cc-env"
-	CC_IMAGE="$(sed -nre 's!.+(docker-registry\.aus\.optiver\.com/[^ ]+/[^ ]+).*!\1!p' "${CC_EXE}" | tail -n1)"
-	mkdir --parents /media/cc-env/opt/ || true
-	${HOME}/dot-files/bin/docker-run.sh -v /media/cc-env:/media/cc-env -u 0 ${CC_IMAGE} rsync -vpPAXrogthlm --delete /opt/optiver/ /media/cc-env/opt/optiver/
 ) >&${log_fd} 2>&${log_fd} &
 disown -h
 disown
