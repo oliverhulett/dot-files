d15c1601ec230c0033a4de66d5787427d4141691
Oliver Hulett <oliver.hulett@optiver.com.au>
2017-04-12 10:22:23 +1000

    Parametise controller so we remove the right one

--- Windoze/sync2home/mail_patches_home.sh
+++ Windoze/sync2home/mail_patches_home.sh
@@ -11,6 +11,7 @@ git pullb
 TODAY="$(date '+%Y-%m-%d')"
 LAST_HASH="$(sed -ne '1p' "${HASH_FILE}")"
 NEXT_HASH="$(git rev-parse HEAD)"
+echo
 echo "Syncing from ${LAST_HASH} to ${NEXT_HASH}"
 
 PATCHES_ROOT="${TODAY}.dotfiles"
@@ -19,10 +20,12 @@ rm -rf "${PATCHES_ROOT}" "${ZIPFILE}" 2>/dev/null
 mkdir "${PATCHES_ROOT}"
 
 NUM_PATCHES=0
+echo
 echo "Using list files: " files.*
 for f in files.*; do
 	d="$(echo $f | sed -re 's/^files.//')"
 	mkdir "${PATCHES_ROOT}/$d" 2>/dev/null
+	echo
 	echo "Formatting patches from ${LAST_HASH} for $f"
 	fmt_cmd="git format-patch --output-directory=${PATCHES_ROOT}/$d --no-binary --attach --to oliver.hulett@gmail.com ${LAST_HASH} -- $(xargs <$f)"
 	echo $fmt_cmd
@@ -30,12 +33,15 @@ for f in files.*; do
 	NUM_PATCHES=$(( NUM_PATCHES + np ))
 done
 if [ $NUM_PATCHES == 0 ]; then
+	echo
 	echo "No patches formatted, not sending e-mail.  Press any key to exit"
 	read -r -n1
 	exit 0
 fi
 
+echo
 echo "Zipping $NUM_PATCHES patches"
+ls -lR "${PATCHES_ROOT}"
 echo "${ZIPPER}" a "${ZIPFILE}" "${PATCHES_ROOT}"
 "${ZIPPER}" a "${ZIPFILE}" "${PATCHES_ROOT}"
 if [ ! -s "${ZIPFILE}" ]; then
@@ -44,15 +50,19 @@ if [ ! -s "${ZIPFILE}" ]; then
 	exit 0
 fi
 
+echo
 echo "Mailing $NUM_PATCHES patches"
 echo "${MAILER}" -smtp unixmail.comp.optiver.com -f olihul@optiver.com.au -t oliver.hulett@gmail.com -sub "${TODAY} dotfiles" -attach "${ZIPFILE}",application/x-7z-compressed,a
 "${MAILER}" -smtp unixmail.comp.optiver.com -f olihul@optiver.com.au -t oliver.hulett@gmail.com -sub "${TODAY} dotfiles" -attach "${ZIPFILE}",application/x-7z-compressed,a
+echo
 echo "Removing $PATCHES_ROOT and $ZIPFILE"
 rm -rf "${PATCHES_ROOT}" "${ZIPFILE}" 2>/dev/null
 
+echo
 echo "Committing last mailed hash: ${NEXT_HASH}"
 echo ${NEXT_HASH} >"${HASH_FILE}"
 git commit .last-mailed-hash -m"Sync2Home autocommit: ${NUM_PATCHES} patches: ${LAST_HASH} to ${NEXT_HASH}" --allow-empty
 git push
+echo
 echo "Done.  Press any key to exit"
 read -r -n1
