779a0c80604a74a47e657672195c67ed2b71f03f
Oliver Hulett <oliver.hulett@optiver.com.au>
2017-04-12 12:21:34 +1000

    Autocommit: Wed, 12 Apr 2017 12:21:34 +1000\n M autocommit.sh  M crontab.3100-centos7dev

--- autocommit.sh
+++ autocommit.sh
@@ -14,3 +14,5 @@ crontab -l >${HOME}/dot-files/crontab.$(hostname -s) && echo -e "\n## Backed up 
 ( rpm -qa | sort; pip freeze | sort ) >${HOME}/dot-files/installed-software.txt
 ## Commit dot-files to git for extra backups.
 ( cd ${HOME}/dot-files && git commit --allow-empty -aqm "Autocommit: $(date -R)\n$(git status --short)" && git pullb && git push -q )
+## Backup a small number of key system-wide configuration files.
+rsync -zPAXrogthlm --delete --files-from=${HOME}/dot-files/backups.txt / ${HOME}/etc/backups
