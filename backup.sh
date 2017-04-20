5801223e48a0fe7dff1a871918780e2bb031882d
Oliver Hulett <oliver.hulett@optiver.com.au>
2017-04-20 11:09:35 +1000

    Autocommit: Thu, 20 Apr 2017 11:09:35 +1000\n M backup.sh  M crontab.3100-centos7dev

--- backup.sh
+++ backup.sh
@@ -6,6 +6,8 @@ source "${HOME}/dot-files/bash_common.sh" 2>/dev/null && eval "${capture_output}
 ${HOME}/dot-files/autocommit.sh
 
 ## Backup a small number of key system-wide configuration files.
-rsync -zPAXrogthlm --delete --files-from=${HOME}/dot-files/backups.txt / ${HOME}/etc/backups
+rsync -PAXrgthlm --delete --files-from=${HOME}/dot-files/backups.txt / ${HOME}/etc/backups
+chown -R olihul ${HOME}/etc/backups
+chmod -R u+rw ${HOME}/etc/backups
 ## Push local configuration and backups to another box.
 rsync -zPAXrogthlm --delete --delete-excluded --exclude=opt/pyvenv ${HOME}/.ssh ${HOME}/etc ${HOME}/opt /H_DRIVE/
