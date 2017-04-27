a3c59c52ce6f5294c8fb7fd6a4a63ba015a8ff22
Oliver Hulett <oliver.hulett@optiver.com.au>
2017-04-27 18:45:54 +1000

    Autocommit: Thu, 27 Apr 2017 18:45:54 +1000\nD  bash_aliases/49-setup-pyvenv.sh  M bin/python_setup.sh  M crontab.3100-centos7dev  M dot-files

--- bash_aliases/49-setup-pyvenv.sh
+++
@@ -1,4 +0,0 @@
-# Set-up python virtual env
-python_setup.sh &
-disown -h 2>/dev/null
-disown 2>/dev/null
--- bin/python_setup.sh
+++ bin/python_setup.sh
@@ -8,12 +8,18 @@ source "${HOME}/.bash_aliases/09-profile.d-pyvenv.sh"
 
 venv_setup
 
+function pip_version()
+{
+	${PYVENV_HOME}/bin/pip --version 2>&1
+}
+
 PYSETUP_MARKER="${PYVENV_HOME}/.setup"
-if [ ! -e "${PYSETUP_MARKER}" ]; then
+if [ ! -e "${PYSETUP_MARKER}" ] || [ "$(command cat "${PYSETUP_MARKER}" 2>/dev/null)" != "$(pip_version)" ]; then
 	touch "${PYSETUP_MARKER}"
-	trap 'rm "${PYSETUP_MARKER}"' EXIT
 	## Install the things
 	pip -q install -U pip
 	pip -q install -U wheel setuptools
 	pip -q install -U -r "${DOTFILES}/python_setup.txt"
+
+	pip_version >"${PYSETUP_MARKER}"
 fi
