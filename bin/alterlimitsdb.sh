938ca9c0c11e5399ffa420f1622eb5b61bda1a80
Oliver Hulett <oliver.hulett@optiver.com.au>
2017-05-05 11:12:58 +1000

    alterlimitsdb tests

--- bin/alterlimitsdb.sh
+++ bin/alterlimitsdb.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
-set -x
 #
 #	Wrapper for the alterlimitsdb python module.
 #
-
 HERE="$(dirname "$(readlink -f "$0")")"
 DOTFILES="$(dirname "${HERE}")"
 source "${DOTFILES}/bash_common.sh" 2>/dev/null && eval "${capture_output}" || true
 
+set -x
+
 if [ -z "$ALTERLIMITSDB_DIR" ]; then
-	clone.sh limits_system limis_server
+	clone.sh limits_system limits_server
 	export ALTERLIMITSDB_DIR="$(get-repo-dir.sh limits_system limits_server alterlimitsdb)"
+fi
+if [ -z "$ALTERLIMITSDB_DIR" ] || [ ! -d "$ALTERLIMITSDB_DIR" ]; then
+	echo "\$ALTERLIMITSDB_DIR is not set or is not a directory: $ALTERLIMITSDB_DIR" >&2
+	exit 1
 fi
 if [ -z "$DB_URI" ]; then
 	export DB_URI='postgresql://operat@devenv002:6002/limitsdb_ml'
