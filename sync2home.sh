0a2491f91ade828600f558fd3e75b354232b52ce
Oliver Hulett <oliver.hulett@optiver.com.au>
2017-05-19 09:45:40 +1000

    Sync with patches, should pick up adds and deletes as well :

--- sync2home.sh
+++ sync2home.sh
@@ -1,7 +1,10 @@
 #!/bin/bash -x
 set -e
 
-cd "$(dirname "${BASH_SOURCE[0]}")" && cd "$(git home)" || exit 1
+HERE="$(dirname "$0")"
+cd "${HERE}" || ( echo "Failed to enter run directory"; exit 1 )
+
+GIT_ROOT="$(git home)"
 
 OTHER_REMOTE="$(git remote | command grep -v origin)"
 if [ -z "${OTHER_REMOTE}" ]; then
@@ -9,8 +12,26 @@ if [ -z "${OTHER_REMOTE}" ]; then
 	exit 0
 fi
 BRANCH="$(git this)"
-
-#trap 'git reset --hard HEAD' EXIT
+git pullb
 git fetch "$OTHER_REMOTE"
-git diff -R "$OTHER_REMOTE/$BRANCH" -- $(cat sync2home.txt) | git apply --index
-#git commit --allow-empty -m"Autocommit diff from $OTHER_REMOTE/$BRANCH on $(date -R)\n$(git status --short)"
+
+IGNORE_LIST="${HERE}/sync2home.ignore.txt"
+HASH_FILE="${HERE}/.sync2home.last-hash"
+echo "${HASH_FILE}" >>"${IGNORE_LIST}"
+echo "$(sort -u "${IGNORE_LIST}" | sed -re '/^$/d')" >"${IGNORE_LIST}"
+LAST_HASH="$(sed -ne '1p' "${HASH_FILE}")"
+NEXT_HASH="$(git rev-parse "$OTHER_REMOTE/$BRANCH")"
+echo
+echo "Syncing from ${LAST_HASH} to ${NEXT_HASH}"
+
+git format-patch --stdout ${LAST_HASH} | git apply --index
+git reset -- $(cat "${IGNORE_LIST}")
+git clean -fd
+
+echo
+echo "Committing last sync-ed hash: ${NEXT_HASH}"
+echo ${NEXT_HASH} >"${HASH_FILE}"
+git commit "$(basename "$HASH_FILE")" -m"Sync2Home autocommit: ${LAST_HASH} to ${NEXT_HASH}" --allow-empty
+git push
+echo
+echo "Done.  Sync-ed $NUM_PATCHES to $OTHER_REMOTE/$BRANCH; from ${LAST_HASH} to ${NEXT_HASH}"
