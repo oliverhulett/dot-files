15027031e1e53765ddb96bca45d26c72b12fa095
Oliver Hulett <oliver.hulett@optiver.com.au>
2017-05-18 13:55:37 +1000

    Add git lg as a wrapped command

---
+++ git_wrappers/log.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+OPTS=$(getopt -o "ac:" --long "all,count:" -n "$(basename -- "$0")" -- "$@")
+es=$?
+if [ $es != 0 ]; then
+	exit $es
+fi
+
+COUNT="--max-count=10"
+eval set -- "${OPTS}"
+while true; do
+	case "$1" in
+		-c | --count )
+			COUNT="--max-count=$2"
+			shift 2
+			;;
+		-a | --all )
+			COUNT=
+			shift
+			;;
+		-- ) shift; break ;;
+		* ) break ;;
+	esac
+done
+
+if echo "$1" | grep -qwE '[0-9]+'; then
+	COUNT="--max-count=$1"
+	shift
+fi
+git log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cd)%C(bold blue)<%an>%Creset' --abbrev-commit $COUNT "$@"
