c22c626bce9a5b76e5410912f4a087ac6b51bb7c
Oliver Hulett <oliver.hulett@optiver.com.au>
2017-05-18 19:50:03 +1000

    undo clobber from optiver

--- bash_aliases/19-env-git.sh
+++ bash_aliases/19-env-git.sh
@@ -1,24 +1,21 @@
 ## Useful git environment setup.
 
-GIT_COMPLETE_FILE="/usr/share/git-core/contrib/completion/git-completion.bash"
-if [ -e "${GIT_COMPLETE_FILE}" ]; then
-	source "${GIT_COMPLETE_FILE}"
-else
-	GIT_COMPLETE_FILE="${HOME}/etc/git-completion.bash"
-	if [ -e "${GIT_COMPLETE_FILE}" ]; then
-		source "${GIT_COMPLETE_FILE}"
+for f in "/etc/bash_completion.d/git-prompt" \
+		 "/usr/share/git-core/contrib/completion/git-completion.bash" \
+		 "${HOME}/etc/git-completion.bash"; do
+	if [ -e "$f" ]; then
+		source "$f"
+		break
 	fi
-fi
-
-GIT_PROMPT_FILE="/usr/share/git-core/contrib/completion/git-prompt.sh"
-if [ -e "${GIT_PROMPT_FILE}" ]; then
-	source "${GIT_PROMPT_FILE}"
-else
-	GIT_PROMPT_FILE="${HOME}/etc/git-prompt.sh"
-	if [ -e "${GIT_PROMPT_FILE}" ]; then
-		source "${GIT_PROMPT_FILE}"
+done
+for f in "/etc/bash_completion.d/git-prompt" \
+		 "/usr/share/git-core/contrib/completion/git-prompt.sh" \
+		 "${HOME}/etc/git-prompt.sh"; do
+	if [ -e "$f" ]; then
+		source "$f"
+		break
 	fi
-fi
+done
 
 export GIT_PS1_SHOWDIRTYSTATE=1
 export GIT_PS1_SHOWUNTRACKEDFILES=1
@@ -29,7 +26,7 @@ function __custom_git_ps1()
 {
 	d="$(git branch --no-color 2>/dev/null | sed -nre 's/^\* //p' | cut -d_ -f1 | sed -re 's!^[^/]+/!!')"
 	wd="$(pwd | sed -re 's!^/home/olihul/!!')"
-	if grep -qw "master" <(echo $wd) 2>&1 >/dev/null || ! grep -qw "$d" <(echo $wd) 2>&1 >/dev/null; then
+	if grep -qw "master" <(echo $wd) >/dev/null 2>&1 || ! grep -qw "$d" <(echo $wd) >/dev/null 2>&1; then
 		__git_ps1 "$@"
 	else
 		__git_ps1 "$@" "%s" | sed -re 's!(\w+:)?[/0-9a-zA-Z_-]+( ?.*)?!\1\2!'
--- setup-home.sh
+++ setup-home.sh
@@ -2,31 +2,15 @@
 source "${HOME}/dot-files/bash_common.sh" 2>/dev/null && eval "${capture_output}" || true
 
 HERE="$(cd "$(dirname "$0")" && pwd -P)"
-RELPATH="${HERE}/bin/relpath.sh"
+RELPATH="/usr/local/bin/relpath.sh"
 
-if ! echo "${HTTP_PROXY}" | grep -q "`whoami`" 2>/dev/null; then
-	source "${HERE}/bash_aliases/19-env-proxy.sh" 2>/dev/null
-	proxy_setup -q
-fi
-
-echo "Updating dot-files..."
-# Can't pull here, you risk changing this file
-( cd "${HERE}" && git submodule init && git submodule sync && git submodule update ) >&${log_fd} &
-disown -h 2>/dev/null
-disown 2>/dev/null
-
-if [ -f "${HERE}/crontab.$(hostname -s)" ]; then
-	echo "Installing crontab from ~/dot-files/crontab.$(hostname -s)..."
-	crontab <(head -n -2 "${HERE}/crontab.$(hostname -s)")
-elif [ -f "${HERE}/crontab" ]; then
-	echo "Installing crontab from ~/dot-files/crontab..."
-	crontab "${HERE}/crontab"
-fi
+chmod +x "${HERE}/src/install" "${HERE}/setup-home.sh" "${HERE}/lessfilter" "${HERE}/tests/run.sh"
+"${HERE}/src/install" -k ${HERE}/src/*.*
 
 DOTFILES=
-if [ -f "${HERE}/dot-files.$(hostname -s)" ]; then
-	echo "Linking dot files from ~/dot-files/dot-files.$(hostname -s)..."
-	DOTFILES="${HERE}/dot-files.$(hostname -s)"
+if [ -f "${HERE}/dot-files.$(hostname -s | tr '[A-Z]' '[a-z]')" ]; then
+	echo "Linking dot files from ~/dot-files/dot-files.$(hostname -s | tr '[A-Z]' '[a-z]')..."
+	DOTFILES="${HERE}/dot-files.$(hostname -s | tr '[A-Z]' '[a-z]')"
 elif [ -f "${HERE}/dot-files" ]; then
 	echo "Linking dot files from ~/dot-files/dot-files..."
 	DOTFILES="${HERE}/dot-files"
@@ -48,7 +32,12 @@ else
 	echo "No dot-files file found, not linking anything..."
 fi
 
-GIT_CREDS="${HOME}/.git-credentials"
-rm "${GIT_CREDS}" 2>/dev/null || true
-echo "https://oliverhulett:$(sed -ne '1p' "${HOME}/etc/passwd.github")@github.com" >>"${GIT_CREDS}"
-chmod 0600 "${GIT_CREDS}"
+if [ -f "${HERE}/crontab.$(hostname -s | tr '[A-Z]' '[a-z]')" ]; then
+	echo "Installing crontab from ~/dot-files/crontab.$(hostname -s | tr '[A-Z]' '[a-z]')..."
+	crontab "${HERE}/crontab.$(hostname -s | tr '[A-Z]' '[a-z]')"
+fi
+
+#GIT_CREDS="${HOME}/.git-credentials"
+#rm "${GIT_CREDS}" 2>/dev/null || true
+#echo "https://oliverhulett:$(sed -ne '1p' "${HOME}/etc/passwd.github")@github.com" >>"${GIT_CREDS}"
+#chmod 0600 "${GIT_CREDS}"
