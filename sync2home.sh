7e3cecff2de26f7c7fdc83423f190c2abf6b54b7
Oliver Hulett <oliver.hulett@gmail.com>
2017-05-18 22:28:56 +1000

    bail from syncing if there's no other remote

--- sync2home.sh
+++ sync2home.sh
@@ -3,10 +3,14 @@ set -e
 
 cd "$(dirname "${BASH_SOURCE[0]}")" && cd "$(git home)" || exit 1
 
-#trap 'git reset --hard HEAD' EXIT
-
 OTHER_REMOTE="$(git remote | command grep -v origin)"
+if [ -z "${OTHER_REMOTE}" ]; then
+	echo "Can't merge; there's no other remote..."
+	exit 0
+fi
 BRANCH="$(git this)"
+
+#trap 'git reset --hard HEAD' EXIT
 git fetch "$OTHER_REMOTE"
 git diff -R "$OTHER_REMOTE/$BRANCH" -- $(cat sync2home.txt) | git apply --index
 #git commit --allow-empty -m"Autocommit diff from $OTHER_REMOTE/$BRANCH on $(date -R)\n$(git status --short)"
