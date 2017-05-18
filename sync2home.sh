cd6305bfeb453f9559888611de298a26f6f1629a
Oliver Hulett <oliver.hulett@optiver.com.au>
2017-05-18 19:04:41 +1000

    Added sync2home v2

---
+++ sync2home.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -x
+set -e
+
+cd "$(dirname "${BASH_SOURCE[0]}")" && cd "$(git home)" || exit 1
+
+trap 'git reset --hard HEAD' EXIT
+
+OTHER_REMOTE="$(git remote | command grep -v origin)"
+BRANCH="$(git this)"
+git fetch "$OTHER_REMOTE"
+git diff -R "$OTHER_REMOTE/$BRANCH" -- $(cat sync2home.txt) | git apply --index
+git commit --allow-empty -m"Autocommit diff from $OTHER_REMOTE/$BRANCH on $(date -R)\n$(git status --short)"
