d3d6ecd690204a05c191972dc970903ae64a6463
Oliver Hulett <oliver.hulett@optiver.com.au>
2017-05-19 09:27:32 +1000

    Merged from home

--- sync2home.sh
+++ sync2home.sh
@@ -3,10 +3,10 @@ set -e
 
 cd "$(dirname "${BASH_SOURCE[0]}")" && cd "$(git home)" || exit 1
 
-#trap 'git reset --hard HEAD' EXIT
+trap 'git reset --hard HEAD' EXIT
 
 OTHER_REMOTE="$(git remote | command grep -v origin)"
 BRANCH="$(git this)"
 git fetch "$OTHER_REMOTE"
 git diff -R "$OTHER_REMOTE/$BRANCH" -- $(cat sync2home.txt) | git apply --index
-#git commit --allow-empty -m"Autocommit diff from $OTHER_REMOTE/$BRANCH on $(date -R)\n$(git status --short)"
+git commit --allow-empty -m"Autocommit diff from $OTHER_REMOTE/$BRANCH on $(date -R)\n$(git status --short)"
