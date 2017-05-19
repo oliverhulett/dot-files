0fb4dc646109a0d9ec83022c71f27cea34cf7b72
Oliver Hulett <oliver.hulett@optiver.com.au>
2017-05-19 10:21:38 +1000

    sync2home

--- sync2home.sh
+++ sync2home.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash
 set -e
 
 HERE="$(dirname "${BASH_SOURCE[0]}")"
@@ -12,6 +12,8 @@ if [ -z "${OTHER_REMOTE}" ]; then
 	exit 0
 fi
 BRANCH="$(git this)"
+
+echo "Pulling and fetching latest from remotes..."
 git pullb
 git fetch "$OTHER_REMOTE"
 
@@ -22,16 +24,14 @@ echo "$(sort -u "${IGNORE_LIST}" | sed -re '/^$/d')" >"${IGNORE_LIST}"
 LAST_HASH="$(sed -ne '1p' "${HASH_FILE}")"
 NEXT_HASH="$(git rev-parse "$OTHER_REMOTE/$BRANCH")"
 echo
-echo "Syncing from ${LAST_HASH} to ${NEXT_HASH}"
+echo "Syncing from ${LAST_HASH} to $OTHER_REMOTE/$BRANCH (${NEXT_HASH})"
 
-git format-patch --stdout -p ${LAST_HASH} | git apply --index --3way $(awk '{print "--exclude=" $0}' "${IGNORE_LIST}")
-#git reset -- $(cat "${IGNORE_LIST}")
-#git clean -fd
+git format-patch --stdout -p ${LAST_HASH}..$OTHER_REMOTE/$BRANCH | git apply --verbose --index --3way $(awk '{print "--exclude=" $0}' "${IGNORE_LIST}")
 
 echo
 echo "Committing last sync-ed hash: ${NEXT_HASH}"
 echo ${NEXT_HASH} >"${HASH_FILE}"
-echo git commit "$(basename "$HASH_FILE")" -m"Sync2Home autocommit: ${LAST_HASH} to ${NEXT_HASH}" --allow-empty
-#git push
+git commit "$(basename "$HASH_FILE")" -m"Sync2Home autocommit: ${LAST_HASH} to ${NEXT_HASH}" --allow-empty
+git push
 echo
-echo "Done.  Sync-ed $NUM_PATCHES to $OTHER_REMOTE/$BRANCH; from ${LAST_HASH} to ${NEXT_HASH}"
+echo "Done.  Sync-ed $OTHER_REMOTE/$BRANCH; from ${LAST_HASH} to ${NEXT_HASH}"
