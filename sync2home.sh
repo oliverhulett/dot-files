4ce788e2928c8d42879371bfce8d8c4fe1683808
Oliver Hulett <oliver.hulett@optiver.com.au>
2017-05-19 10:30:21 +1000

    sync2home

--- sync2home.sh
+++ sync2home.sh
@@ -1,7 +1,8 @@
-#!/bin/bash -x
+#!/bin/bash
 set -e
 
-cd "$(dirname "${BASH_SOURCE[0]}")" && cd "$(git home)" || exit 1
+HERE="$(dirname "${BASH_SOURCE[0]}")"
+cd "${HERE}" || ( echo "Failed to enter run directory"; exit 1 )
 
 OTHER_REMOTE="$(git remote | command grep -v origin)"
 if [ -z "${OTHER_REMOTE}" ]; then
@@ -10,7 +11,25 @@ if [ -z "${OTHER_REMOTE}" ]; then
 fi
 BRANCH="$(git this)"
 
-#trap 'git reset --hard HEAD' EXIT
+echo "Pulling and fetching latest from remotes..."
+git pullb
 git fetch "$OTHER_REMOTE"
-git diff -R "$OTHER_REMOTE/$BRANCH" -- $(cat sync2home.txt) | git apply --index
-#git commit --allow-empty -m"Autocommit diff from $OTHER_REMOTE/$BRANCH on $(date -R)\n$(git status --short)"
+
+IGNORE_LIST="${HERE}/sync2home.ignore.txt"
+HASH_FILE="${HERE}/.sync2home.last-hash"
+echo "$(basename "${HASH_FILE}")" >>"${IGNORE_LIST}"
+echo "$(sort -u "${IGNORE_LIST}" | sed -re '/^$/d')" >"${IGNORE_LIST}"
+LAST_HASH="$(sed -ne '1p' "${HASH_FILE}")"
+NEXT_HASH="$(git rev-parse "$OTHER_REMOTE/$BRANCH")"
+echo
+echo "Syncing from ${LAST_HASH} to $OTHER_REMOTE/$BRANCH (${NEXT_HASH})"
+
+git format-patch --stdout -p ${LAST_HASH}..$OTHER_REMOTE/$BRANCH | git apply --verbose --index --3way $(awk '{print "--exclude=" $0}' "${IGNORE_LIST}")
+
+echo
+echo "Committing last sync-ed hash: ${NEXT_HASH}"
+echo ${NEXT_HASH} >"${HASH_FILE}"
+git commit -a -m"Sync2Home autocommit: ${LAST_HASH} to ${NEXT_HASH}" --allow-empty
+#git push
+echo
+echo "Done.  Sync-ed $OTHER_REMOTE/$BRANCH; from ${LAST_HASH} to ${NEXT_HASH}"
