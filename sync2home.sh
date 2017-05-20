6c7727bd535490dc6646d996802675968cc0ce61
Oliver Hulett <oliver.hulett@gmail.com>
2017-05-20 20:59:20 +1000

    sync by merging...

--- sync2home.sh
+++ sync2home.sh
@@ -10,27 +10,17 @@ if [ -z "${OTHER_REMOTE}" ]; then
 	exit 0
 fi
 BRANCH="$(git this)"
+IGNORE_LIST="${HERE}/sync2home.ignore.txt"
 
 echo "Pulling and fetching latest from remotes..."
-git pullb
-git fetch "$OTHER_REMOTE"
-
-IGNORE_LIST="${HERE}/sync2home.ignore.txt"
-HASH_FILE="${HERE}/.sync2home.last-hash"
-echo "$(basename "${HASH_FILE}")" >>"${IGNORE_LIST}"
-echo "$(sort -u "${IGNORE_LIST}" | sed -re '/^$/d')" >"${IGNORE_LIST}"
-LAST_HASH="$(sed -ne '1p' "${HASH_FILE}")"
-NEXT_HASH="$(git rev-parse "$OTHER_REMOTE/$BRANCH")"
+git push
+git pull --all
+git fetch "$OTHER_REMOTE" "$BRANCH"
+echo "Merging from $OTHER_REMOTE/$BRANCH..."
+git merge --no-commit FETCH_HEAD
+git reset -- $(cat "$IGNORE_LIST")
+echo "Committing changes..."
+git status
+git commit -a -m"Sync2Home autocommit from $OTHER_REMOTE/$BRANCH: $(git status -s)" --allow-empty
 echo
-echo "Syncing from ${LAST_HASH} to $OTHER_REMOTE/$BRANCH (${NEXT_HASH})"
-
-echo git format-patch --stdout -p ${LAST_HASH}..$OTHER_REMOTE/$BRANCH
-git format-patch --stdout -p ${LAST_HASH}..$OTHER_REMOTE/$BRANCH | git apply --verbose --index --3way $(awk '{print "--exclude=" $0}' "${IGNORE_LIST}")
-
-echo
-echo "Committing last sync-ed hash: ${NEXT_HASH}"
-echo ${NEXT_HASH} >"${HASH_FILE}"
-git commit -a -m"Sync2Home autocommit: ${LAST_HASH} to ${NEXT_HASH}" --allow-empty
-#git push
-echo
-echo "Done.  Sync-ed $OTHER_REMOTE/$BRANCH; from ${LAST_HASH} to ${NEXT_HASH}"
+echo "Done.  Sync-ed $OTHER_REMOTE/$BRANCH"
