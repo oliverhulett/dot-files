8df144bf927c210ffb215f55f230374c382848b2
Oliver Hulett <oliver.hulett@gmail.com>
2017-05-22 22:19:56 +1000

    fix

--- git_wrappers/cleanbranches.sh
+++ git_wrappers/cleanbranches.sh
@@ -2,12 +2,13 @@
 ## Called from the top level of the git repository.
 set -e
 
-shopt -s nullglob
 test -d .git
 
+shopt -s nullglob
 find .git/refs/heads -type f | while read -r; do
 	set -- .git/refs/remotes/*/"${REPLY#./}"
-	if [ $# -eq 0 ] && [ "${REPLY#./}" != "master" ]; then
-		git branch -d "${REPLY#./}"
+	b="$(basename "${REPLY}")"
+	if [ $# -eq 0 ] && [ "$b" != "master" ]; then
+		git branch -d "$b"
 	fi
 done
