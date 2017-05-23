90830ebba7c7cf87c66c0851e16171803a5a842e
Oliver Hulett <oliver.hulett@optiver.com.au>
2017-05-23 15:29:45 +1000

    Update windoze proxy

--- Windoze/bin/proxy_setup.bat
+++ Windoze/bin/proxy_setup.bat
@@ -1,4 +1,4 @@
 @echo off
 set /p passwd=<C:\Users\olihul\passwd
-set HTTP_PROXY=http://olihul:%passwd%@sydproxy.comp.optiver.com:8080
-set HTTPS_PROXY=http://olihul:%passwd%@sydproxy.comp.optiver.com:8080
+set HTTP_PROXY=http://olihul:%passwd%@sydsquid.aus.optiver.com:3128
+set HTTPS_PROXY=http://olihul:%passwd%@sydsquid.aus.optiver.com:3128
