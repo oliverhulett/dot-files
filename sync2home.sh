1fe0287e658688d7bebe54744fe73973022bb5a9
Oliver Hulett <oliver.hulett@optiver.com.au>
2017-05-23 15:27:14 +1000

    things from home

--- sync2home.sh
+++ sync2home.sh
@@ -4,34 +4,23 @@ set -e
 HERE="$(dirname "${BASH_SOURCE[0]}")"
 cd "${HERE}" || ( echo "Failed to enter run directory"; exit 1 )
 
-GIT_ROOT="$(git home)"
-
 OTHER_REMOTE="$(git remote | command grep -v origin)"
 if [ -z "${OTHER_REMOTE}" ]; then
 	echo "Can't merge; there's no other remote..."
 	exit 0
 fi
 BRANCH="$(git this)"
+IGNORE_LIST="${HERE}/sync2home.ignore.txt"
 
 echo "Pulling and fetching latest from remotes..."
-git pullb
-git fetch "$OTHER_REMOTE"
-
-IGNORE_LIST="${HERE}/sync2home.ignore.txt"
-HASH_FILE="${HERE}/.sync2home.last-hash"
-echo "$(basename "${HASH_FILE}")" >>"${IGNORE_LIST}"
-echo "$(sort -u "${IGNORE_LIST}" | sed -re '/^$/d')" >"${IGNORE_LIST}"
-LAST_HASH="$(sed -ne '1p' "${HASH_FILE}")"
-NEXT_HASH="$(git rev-parse "$OTHER_REMOTE/$BRANCH")"
+git push
+git pull --all
+git fetch "$OTHER_REMOTE" "$BRANCH"
+echo "Merging from $OTHER_REMOTE/$BRANCH..."
+git merge --no-commit FETCH_HEAD
+git reset -- $(cat "$IGNORE_LIST")
+echo "Committing changes..."
+git status
+git commit -a -m"Sync2Home autocommit from $OTHER_REMOTE/$BRANCH: $(git status -s)" --allow-empty
 echo
-echo "Syncing from ${LAST_HASH} to $OTHER_REMOTE/$BRANCH (${NEXT_HASH})"
-
-git format-patch --stdout -p ${LAST_HASH}..$OTHER_REMOTE/$BRANCH | git apply --verbose --index --3way $(awk '{print "--exclude=" $0}' "${IGNORE_LIST}")
-
-echo
-echo "Committing last sync-ed hash: ${NEXT_HASH}"
-echo ${NEXT_HASH} >"${HASH_FILE}"
-git commit -a -m"Sync2Home autocommit: ${LAST_HASH} to ${NEXT_HASH}" --allow-empty
-#git push
-echo
-echo "Done.  Sync-ed $OTHER_REMOTE/$BRANCH; from ${LAST_HASH} to ${NEXT_HASH}"
+echo "Done.  Sync-ed $OTHER_REMOTE/$BRANCH"
