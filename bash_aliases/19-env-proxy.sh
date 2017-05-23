d8a3e699d6cac2048481c3986031e7bf73c40c10
Oliver Hulett <oliver.hulett@optiver.com.au>
2017-05-23 13:27:55 +1000

    Add tunnel mode to proxy

--- bash_aliases/19-env-proxy.sh
+++ bash_aliases/19-env-proxy.sh
@@ -1,7 +1,5 @@
-PROXY_URL="sydproxy.comp.optiver.com"
-#PROXY_URL="sydsquid.aus.optiver.com"
-PROXY_PORT=":8080"
-#PROXY_PORT=":3128"
+PROXY_URL="sydsquid.aus.optiver.com"
+PROXY_PORT=":3128"
 export HTTP_PROXY="http://${PROXY_URL}${PROXY_PORT}"
 export HTTPS_PROXY="https://${PROXY_URL}${PROXY_PORT}"
 export http_proxy="${HTTP_PROXY}"
@@ -11,6 +9,13 @@ unset https_proxy_orig
 
 export NO_PROXY='*.comp.optiver.com,*.aus.optiver.com,127.0.0.1,localhost,git,srcsyd.comp.optiver.com,10.0.2.*,192.168.56.*'
 export no_proxy="${NO_PROXY}"
+
+LOCAL_PROXY_URL="localhost"
+LOCAL_PROXY_PORT=":54321"
+TUNNEL_PROXY_URL="sydproxy.comp.optiver.com"
+TUNNEL_PROXY_PORT=":8080"
+PROXY_CTRL_SOCK="${HOME}/.proxy-tunnel-ctl-sock"
+PROXY_TUNNEL_DEST="${USER}@${USER}.devsrv"
 
 function urlencode()
 {
@@ -42,6 +47,7 @@ function urldecode()
 
 function proxy_setup()
 {
+	TUNNEL="n"
 	QUIET="n"
 	for a in "$@"; do
 		if id $a >/dev/null 2>/dev/null; then
@@ -55,6 +61,14 @@ function proxy_setup()
 				*q*)
 					QUIET="y"
 					a="${a//q/}"
+					;;
+				*t*)
+					TUNNEL="y"
+					a="${a//t/}"
+					;;
+				*d*)
+					TUNNEL="n"
+					a="${a//d/}"
 					;;
 			esac
 			if [ "$p" == "$a" ]; then
@@ -81,9 +95,20 @@ function proxy_setup()
 	if [ -z "${https_proxy_orig}" ]; then
 		export https_proxy_orig="${https_proxy}"
 	fi
-	export http_proxy="${http_proxy_orig/\/\////$(urlencode "$USER"):$(urlencode "$PASSWD")@}"
+	if [ -e "${PROXY_CTRL_SOCK}" ]; then
+		ssh -S "${PROXY_CTRL_SOCK}" -O exit ${PROXY_TUNNEL_DEST} EXIT
+	fi
+	if [ "$TUNNEL" == "n" ]; then
+		HTTP_PROXY="http://${PROXY_URL}${PROXY_PORT}"
+		HTTPS_PROXY="https://${PROXY_URL}${PROXY_PORT}"
+	else
+		ssh -fNM -S "${PROXY_CTRL_SOCK}" -L ${LOCAL_PROXY_URL}${LOCAL_PROXY_PORT}:${TUNNEL_PROXY_URL}${TUNNEL_PROXY_PORT} ${PROXY_TUNNEL_DEST}
+		HTTP_PROXY="http://${LOCAL_PROXY_URL}${LOCAL_PROXY_PORT}"
+		HTTPS_PROXY="https://${LOCAL_PROXY_URL}${LOCAL_PROXY_PORT}"
+	fi
+	export http_proxy="${HTTP_PROXY/\/\////$(urlencode "$USER"):$(urlencode "$PASSWD")@}"
 	export HTTP_PROXY="${http_proxy}"
-	export https_proxy="${https_proxy_orig/\/\////$(urlencode "$USER"):$(urlencode "$PASSWD")@}"
+	export https_proxy="${HTTPS_PROXY/\/\////$(urlencode "$USER"):$(urlencode "$PASSWD")@}"
 	export HTTPS_PROXY="${https_proxy}"
 	if [ "$QUIET" == "n" ]; then
 		update_config=""
