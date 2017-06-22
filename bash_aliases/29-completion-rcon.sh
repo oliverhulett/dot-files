fc9e0a725734afd5a91e2d9d78c337bfdf57387b
Oliver Hulett <oliver.hulett@optiver.com.au>
2017-06-22 16:55:10 +1000

    test swd and rcon completion

--- bash_aliases/29-completion-rcon.sh
+++ bash_aliases/29-completion-rcon.sh
@@ -1,14 +1,17 @@
+SWD_REPO="${HOME}/StagingCentralizedRepo"
 function expand_rcon_job()
 {
-	RCON_CONF="/apps/centralConfig/rcon.conf"
-	if [ -e "${RCON_CONF}" ]; then
+	if [ -e "${SWD_REPO}" ]; then
 		prefix="${COMP_WORDS[COMP_CWORD]}"
 		if [ ${COMP_CWORD} == 1 ]; then
-			rcon_jobs=`cut -d: -f2 "${RCON_CONF}" | command grep -v ^#`
-			COMPREPLY=($(compgen -W "${rcon_jobs} all" -- $prefix ))
+			shopt -s nullglob
+			for RCON_CONF in "${SWD_REPO}"/*/rcon.conf; do
+				rcon_jobs="${rcon_jobs} $(command grep -vE '^\s*(#|$)' "${RCON_CONF}" | cut -d: -f2 | xargs)"
+			done
+			COMPREPLY=( $(compgen -W "${rcon_jobs} all" -- $prefix) )
 		else
 			if [ ${COMP_CWORD} == 2 ]; then
-				COMPREPLY=($(compgen -W "start stop restart status batchstart tail version blackout get set list get_affinity set_affinity" -- $prefix ))
+				COMPREPLY=( $(compgen -W "start stop restart status batchstart tail version blackout get set list get_affinity set_affinity" -- $prefix) )
 			fi
 		fi
 	fi
@@ -17,10 +20,9 @@ complete -F expand_rcon_job rcon
 
 function expand_swd_job()
 {
-	PROD_REPO="/home/`whoami`/StagingCentralizedRepo"
-	if [ -e ${PROD_REPO} ]; then
+	if [ -e ${SWD_REPO} ]; then
 		prefix="${COMP_WORDS[COMP_CWORD]}"
-		colos=`command ls ${PROD_REPO} | command grep -v -E '(bin|conf|test)'`
+		colos="$(command ls ${SWD_REPO} | command grep -v -E '(bin|conf|test)' | xargs)"
 		COMPREPLY=( $(compgen -W "${colos}" -- ${prefix}) )
 	fi
 }
