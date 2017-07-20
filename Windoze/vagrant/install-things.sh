5199d296af07d573d373bc81de767cea1cd9692b
Oliver Hulett <oliver.hulett@optiver.com.au>
2017-07-20 18:50:03 +1000

    Autocommit: Thu, 20 Jul 2017 18:50:03 +1000\n M Windoze/vagrant/install-things.sh  M crontab.3100-centos7dev  M installed-software.txt

--- Windoze/vagrant/install-things.sh
+++ Windoze/vagrant/install-things.sh
@@ -28,13 +28,13 @@ echo
 echo "Restoring local installs and other backups..."
 
 for d in /H_DRIVE/*; do
-	d="$(basename -- $d)"
-	rsync -vrl /H_DRIVE/$d ${HOME}/
-	find ${HOME}/$d -type f -print0 | xargs -tr0 chmod -x
-	chown -R ${USER} ${HOME}/$d
-	chgrp -R users ${HOME}/$d
+	d="$(basename -- "$d")"
+	rsync -vrl "/H_DRIVE/$d" "${HOME}/"
+	find "${HOME}/$d" -type f -print0 | xargs -tr0 chmod -x
+	chown -R "${USER}" "${HOME}/$d"
+	chgrp -R users "${HOME}/$d"
 done
-chmod -v +x ${HOME}/opt/pyvenv/bin/* ${HOME}/opt/eclipse/eclipse ${HOME}/opt/sublime_text_3/sublime_text ${HOME}/opt/subl.sh ${HOME}/opt/clion-2016.3.2/bin/clion.sh
+chmod -v +x "${HOME}/opt/pyvenv/bin/"* "${HOME}/opt/eclipse/eclipse" "${HOME}/opt/sublime_text_3/sublime_text" "${HOME}/opt/subl.sh" "${HOME}/opt/clion-2016.3.2/bin/clion.sh"
 
 echo
 echo "Installing some things I don't want to docker all the time: yum..."
@@ -56,6 +56,11 @@ PIP_CONFIG_FILE="${HOME}/dot-files/pip.conf" sudo pip install -U setuptools whee
 PIP_CONFIG_FILE="${HOME}/dot-files/pip.conf" sudo pip install pygments flake8
 
 echo
+echo "Running python_setup.sh..."
+rm -rf "${HOME}/opt/pyvenv/"
+"${HOME}/dot-files/bin/python_setup.sh"
+
+echo
 echo "Installing some more things I don't want to docker all the time..."
 
 "$(dirname "$0")"/install-things-w-custom-proxy.sh "$@"
@@ -68,7 +73,7 @@ export PATH="$(prepend_path "${HOME}/dot-files/bin" "/optiver/bin")"
 CC_EXE="/usr/local/bin/cc-env"
 CC_IMAGE="$(sed -nre 's!.+(docker-registry\.aus\.optiver\.com/[^ ]+/[^ ]+).*!\1!p' "${CC_EXE}" | tail -n1)"
 sudo mkdir --parents /media/cc-env/opt/ || true
-${HOME}/dot-files/bin/docker-run.sh -v /media/cc-env:/media/cc-env -u 0 ${CC_IMAGE} rsync -vpPAXrogthlm --delete /opt/optiver /media/cc-env/opt/
+"${HOME}/dot-files/bin/docker-run.sh" -v /media/cc-env:/media/cc-env -u 0 "${CC_IMAGE}" rsync -vpPAXrogthlm --delete /opt/optiver /media/cc-env/opt/
 
 echo
 echo "Done"
