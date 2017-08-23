80fe2581f11859efb4b6a3f771c60c2470568650
Oliver Hulett <oliver.hulett@optiver.com.au>
2017-08-24 08:23:15 +1000

    Update links

--- bash_aliases/49-setup-proxy.sh
+++ bash_aliases/49-setup-proxy.sh
@@ -1,4 +1,4 @@
 if [ -e "${HOME}/.bash_aliases/19-env-proxy.sh" ] && ! echo "${HTTP_PROXY}" | grep -q "$(whoami)" 2>/dev/null; then
 	source "${HOME}/.bash_aliases/19-env-proxy.sh" 2>/dev/null
-	proxy_setup -qp >/dev/null 2>/dev/null
+	proxy_setup -q >/dev/null 2>/dev/null
 fi
