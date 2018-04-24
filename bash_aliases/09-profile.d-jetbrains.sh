cfab28dd3a87aa055743d3fafc2a0d2283091694
Oliver Hulett <ohulett@c02w104ahtdg.office.atlassian.com>
2018-04-24 15:00:02 +1000

    Autocommit from C02W104AHTDG: 3 files changed
     M bash_aliases/09-profile.d-jetbrains.sh
    ?? bin/clone.sh
    ?? bin/get-repo-dir.sh

--- bash_aliases/09-profile.d-jetbrains.sh
+++ bash_aliases/09-profile.d-jetbrains.sh
@@ -2,4 +2,4 @@
 ## Add IntelliJ command line tools to PATH
 # shellcheck disable=SC1090
 source "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")/../bash_common.sh"
-export PATH="$(append_path "${PATH}" "/Applications/IntelliJ IDEA.app/Contents/bin")"
+export PATH="$(append_path "${PATH}" "/Applications/IntelliJ_IDEA.app/Contents/bin")"
