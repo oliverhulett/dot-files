6edc37f12a861826a9001e8444a22d01331f4fa0
Oliver Hulett <ohulett@atlassian.com>
2018-05-09 10:00:02 +1000

    Autocommit from C02W104AHTDG: 1 files changed (2018-05-09 10:00:02)
     M git_wrappers/cleanignored.sh

--- git_wrappers/cleanignored.sh
+++ git_wrappers/cleanignored.sh
@@ -6,7 +6,7 @@ source "${DOTFILES}/bash_common.sh" 2>/dev/null && eval "${capture_output}" || t
 
 echo "Saving known ignored files."
 tmp="$(mktemp -d)"
-find ./ -xdev -not \( -name '.git' -prune -or -name '.svn' -prune \) \( -name .project -or -name .pydevproject -or -name .cproject -or -name .settings -or -name .idea \) -print0 | xargs -0 cp --parents -vxPr --target-directory="${tmp}/" 2>/dev/null
+find ./ -xdev -not \( -name '.git' -prune -or -name '.svn' -prune \) \( -name .project -or -name .pydevproject -or -name .cproject -or -name .settings -or -name .idea -or -name '*.iml' \) -print0 | xargs -0 cp --parents -vxPr --target-directory="${tmp}/" 2>/dev/null
 
 echo "Cleaning ignored files."
 git clean -f -X -d
