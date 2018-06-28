e04320e4a7272f191c2de9aac2e012aa7203e98f
Oliver Hulett <ohulett@atlassian.com>
2018-06-28 12:00:02 +1000

    Autocommit from C02W104AHTDG: 1 files changed (2018-06-28 12:00:02)
     M bash-aliases/39-aliases-atlassian.sh

--- bash-aliases/39-aliases-atlassian.sh
+++ bash-aliases/39-aliases-atlassian.sh
@@ -1,5 +1,6 @@
 # shellcheck shell=bash
 ## Atlassian aliases
 alias tricorder='docker pull docker.atl-paas.net/ath; docker run --rm docker.atl-paas.net/ath | sh'
+alias docker-clock='docker run --rm --privileged alpine hwclock -s'
 
 export MAVEN_OPTS="${MAVEN_OPTS} -Djansi.force=true"
