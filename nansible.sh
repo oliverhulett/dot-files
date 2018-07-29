0f54eeeb6e1e1214dc5a01176f1ab57746b44faa
Oliver Hulett <oliver.hulett@gmail.com>
2018-07-29 23:00:01 +1000

    Autocommit from Odysseus: 3 files changed (2018-07-31 22:00:01)
     M beets.config.yaml
    A  bin/ondemdansible.sh
    D  nansible.sh

--- nansible.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-## Not-an-ansible file.  To be run regularly, it will attempt to make the machine state match the decription (in related files)
-
-HERE="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
-
-source "${HERE}/lib/script-utils.sh"
-
-reentrance_check
