a740be5093322b8eecced5f4ff2451a8c415ae7c
Oliver Hulett <ohulett@atlassian.com>
2018-08-25 00:00:10 +1000

    Autocommit from C02W104AHTDG: 1 files changed (2018-08-25 00:00:10)
     M bash-aliases/19-env-shellcheck.sh

--- bash-aliases/19-env-shellcheck.sh
+++ bash-aliases/19-env-shellcheck.sh
@@ -2,10 +2,10 @@
 ## Ignore some shell check errors...
 SHELLCHECK_OPTS=
 
-#SHELLCHECK_OPTS+=' -e SC1090'	## Can't follow non-const source; use `# shellcheck source=...` to specify.
-#SHELLCHECK_OPTS+=' -e SC1091'	## Not following; file not found, no permissions, or not allowed via -x.
 #SHELLCHECK_OPTS+=' -e SC2155'	## Declare and asign separately to avoid masking return values.
 
+SHELLCHECK_OPTS+=' -e SC1090'	## Can't follow non-const source; use `# shellcheck source=...` to specify.
+SHELLCHECK_OPTS+=' -e SC1091'	## Not following; file not found, no permissions, or not allowed via -x.
 SHELLCHECK_OPTS+=' -e SC2015'	## A && B || C is not if-then-else.
 
 export SHELLCHECK_OPTS
