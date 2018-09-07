95ed96cb5bed26f6ae6d3dfcb9b154584ce305d6
Oliver Hulett <ohulett@atlassian.com>
2018-09-07 17:00:02 +1000

    Autocommit from C02W104AHTDG: 2 files changed (2018-09-07 17:00:02)
     M bash-aliases/39-aliases-atlassian.sh
     M dot-files-common

--- bash-aliases/39-aliases-atlassian.sh
+++ bash-aliases/39-aliases-atlassian.sh
@@ -4,7 +4,7 @@ alias tricorder='docker pull docker.atl-paas.net/ath; docker run --rm docker.atl
 alias docker-clock='docker run --rm --privileged alpine hwclock -s'
 alias vgrok='ngrok start jira-exploratory-development &'
 ## Actually run vgrok so that it is "always" running
-vgrok
+( exec >/dev/null 2>/dev/null; vgrok )
 
 ## JMake doesn't like MAVEN_OPTS being set.  :(
 #export MAVEN_OPTS="${MAVEN_OPTS} -Djansi.force=true"
