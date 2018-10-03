cacd2bde4726f6991b0e606f3cbd192080981f09
Oliver Hulett <ohulett@atlassian.com>
2018-10-03 15:00:01 +1000

    Autocommit from C02W104AHTDG: 3 files changed (2018-10-03 15:00:00)
     M bash-aliases/09-profile.d-atlassian.sh
     M bash-aliases/09-profile.d-brew.sh
     M bash-aliases/09-profile.d-python_virtualenv.sh

--- bash-aliases/09-profile.d-atlassian.sh
+++ bash-aliases/09-profile.d-atlassian.sh
@@ -2,20 +2,9 @@
 # Things for the mac.
 # shellcheck disable=SC2155,SC1090
 
-# Alias gnu utils installed on the mac with homebrew to their usual names.
-## Do we need to detect mac-ness?
-## This should work on linux too (mostly it'll be a no-op, worst case it create some useless links)
-## There is some risk that this doesn't happen early enough.  As long as it does eventually happen though, subsequent loads should work.
-(
-	for f in /usr/local/bin/g*; do
-		g="$(basename -- "$f")"
-		if [ "$g" != 'g[' ] && [ ! -e "/usr/local/bin/${g:1}" ]; then
-			( cd /usr/local/bin/ && ln -s "$g" "${g:1}" 2>/dev/null )
-		fi
-	done
-	# Doesn't work, for some reason.
-	rm '/usr/local/bin/[' 2>/dev/null || true
-) &
+if reentered "${HOME}/.bash-aliases/09-profile.d-atlassian.sh"; then
+	return 0
+fi
 
 export JAVA_HOME="$(/usr/libexec/java_home -v 1.8)"
 export NVM_DIR="${HOME}/.nvm"
--- bash-aliases/09-profile.d-python_virtualenv.sh
+++ bash-aliases/09-profile.d-python_virtualenv.sh
@@ -10,18 +10,3 @@ if [ -d "/usr/local/opt/python/libexec/bin" ]; then
 	source "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")/../bash-common.sh"
 	PATH="$(prepend_path "${PATH}" "/usr/local/opt/python/libexec/bin")"
 fi
-# Add ~/opt/py{3,2.7}venv/bin to the path.  It is where I create my "global" python venv for systems that need these sorts of things.
-function setpy2()
-{
-	deactivate 2>/dev/null
-	# shellcheck disable=SC1090
-	source "${HOME}/opt/py27venv/bin/activate"
-}
-alias setpy2.7=setpy2
-alias setpy27=setpy2
-function setpy3()
-{
-	deactivate 2>/dev/null
-	# shellcheck disable=SC1090
-	source "${HOME}/opt/py3venv/bin/activate"
-}
