4bdf1be42d6d84a44255c6af61a6e7231adb8e68
Oliver Hulett <ohulett@atlassian.com>
2018-10-10 15:00:02 +1100

    Autocommit from C02W104AHTDG: 4 files changed (2018-10-10 15:00:02)
     M bash-aliases/35-aliases-atlassian.sh
     M dot-files-common
     M gitconfig
    A  bash-aliases/25-completion-pipenv.sh

--- bash-aliases/35-aliases-atlassian.sh
+++ bash-aliases/35-aliases-atlassian.sh
@@ -7,3 +7,14 @@ alias vgrok='ngrok start jira-exploratory-development &'
 
 ## JMake doesn't like MAVEN_OPTS being set.  :(
 #export MAVEN_OPTS="${MAVEN_OPTS} -Djansi.force=true"
+
+function jirareleased()
+{
+	repo jira || return 1
+	git pullme
+	echo
+	HASH="${1:-$(git mylasthash)}"
+	echo "Branches containing ${HASH}"
+	git branch --contains "${HASH}"
+	cd - || return 1
+}
