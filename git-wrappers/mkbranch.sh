83fbd43419681e4f882b23a5c102075d3b622be5
Oliver Hulett <ohulett@atlassian.com>
2019-01-25 12:00:03 +1100

    Autocommit from C02W104AHTDG: 3 files changed (2019-01-25 12:00:02)
     M git-wrappers/mkbranch.sh
     M gitconfig
    D  bin/git-new-workdir

--- git-wrappers/mkbranch.sh
+++ git-wrappers/mkbranch.sh
@@ -4,69 +4,13 @@ HERE="$(dirname "$(readlink -f "$0")")"
 DOTFILES="$(dirname "${HERE}")"
 source "${DOTFILES}/bash-common.sh" 2>/dev/null && eval "${capture_output}" || true
 
-CURR_BRANCH="$(git branch --no-color | sed -nre 's/^\* //p')"
-CURR_DIR="$(basename -- "$(pwd)")"
-
 function print_help()
 {
-	echo "git mkbranch [-f] <NEW_TICKET> [<NEW_DESCR>]"
+	echo "git mkbranch <NEW_BRANCH_NAME>"
 	exit 1
 }
 
-if [ "$1" == "-h" -o "$1" == "--help" -o "$1" == "-?" ]; then
-	print_help
-fi
-
-FEATURE="false"
-if [ "$1" == "-f" ]; then
-	FEATURE="true"
-	shift
-fi
-
-NEW_TICKET="$(echo "$1" | cut -d_ -f1)"
-if [ "$NEW_TICKET" != "$1" ]; then
-	NEW_DESCR="$(echo "$1" | cut -d_ -f2-)"
-else
-	NEW_DESCR="$2"
-	shift
-fi
-shift
-NEW_TICKET="$(echo "${NEW_TICKET}" | tr '[:lower:]' '[:upper:]')"
-
-if [ "$FEATURE" == "true" ]; then
-	NEW_BRANCH="feature/$NEW_TICKET"
-else
-	NEW_BRANCH="$USER/$NEW_TICKET"
-fi
-if [ -n "$NEW_DESCR" ]; then
-	NEW_BRANCH="${NEW_BRANCH}_${NEW_DESCR}"
-fi
-
-## MASTER_DIR should be a sibling of each branch's directory, and called master.
-MASTER_DIR="$(dirname "$(pwd)")/master"
-if [ ! -d "$MASTER_DIR" ]; then
-	## Or MASTER_DIR should be a sibling of each branch's directory, and with the same name as the repository.
-	parent="$(dirname "$(pwd)")"
-	MASTER_DIR="$(dirname "${parent}")/$(git remote show origin | grep 'Fetch URL:' | sed 's#^.*/\(.*\).git#\1#')"
-fi
-if [ ! -d "$MASTER_DIR" ]; then
-	## Or MASTER_DIR should be a sibling of each branch's directory, and with the same name as the parent directory.
-	parent="$(dirname "$(pwd)")"
-	MASTER_DIR="$(dirname "${parent}")/$(basename -- "${parent}")"
-fi
-if [ ! -d "$MASTER_DIR" ]; then
-	## Or MASTER_DIR falls back to being CURR_DIR.
-	MASTER_DIR="${CURR_DIR}"
-fi
-
-## This script is designed to be called as a git alias, so we should be in the root of the checkout from which we want to branch.
-NEW_DIR="$NEW_TICKET"
-if [ "$1" != "" ]; then
-	NEW_DIR="$1"
-	shift
-fi
-if [ -e "../$NEW_DIR" ]; then
-	echo "New branch directory already exists.  CD into it and run 'git checkout -b $NEW_BRANCH' like everyone else"
+if [ "$1" == "-h" ] || [ "$1" == "--help" ] || [ "$1" == "-?" ]; then
 	print_help
 fi
 
@@ -77,32 +21,35 @@ else
 	sleep 1
 fi
 
-git pullme --force
-git fetch origin ${CURR_BRANCH}
+CURR_BRANCH="$(git this)"
+CURR_DIR="$(basename -- "$(pwd)")"
+NEW_BRANCH="$1"
+NEW_TICKET="$(git ticket "${NEW_BRANCH}")"
+NEW_DIR="$NEW_TICKET"
 
-( cd .. && git new-workdir "${MASTER_DIR}" "${NEW_DIR}" "${CURR_BRANCH}" )
+## This script is designed to be called as a git alias, so we should be in the root of the checkout from which we want to branch.
+if [ -e "../$NEW_DIR" ]; then
+	echo "New branch directory already exists.  CD into it and run 'git checkout -b $NEW_BRANCH' like everyone else"
+	print_help
+fi
 
-function cleanup()
+function setup_new_worktree()
 {
-	popd
+	shopt -s nullglob
+	set -- ../.[a-z]*
+	if [ "$#" -gt 0 ]; then
+		ln -sv ../.[a-z]* ./ 2>/dev/null
+	fi
+	rm .project 2>/dev/null
+	if [ -f ../.project ] || [ -f ../master/.project ]; then
+		cp ../.project ./ 2>/dev/null || cp ../master/.project ./ 2>/dev/null
+	fi
+	if [ -f .project ]; then
+		sed -re 's!@master</name>!@'"${NEW_TICKET}_${NEW_DESCR}"'</name>!' .project -i
+	fi
 }
-trap cleanup EXIT
 
-pushd "../${NEW_DIR}" >/dev/null
-
-git checkout -b "${NEW_BRANCH}" || git checkout "${NEW_BRANCH}"
-git push --set-upstream origin "${NEW_BRANCH}"
-git update
-
-shopt -s nullglob
-set -- ../.[a-z]*
-if [ "$#" -gt 0 ]; then
-	ln -sv ../.[a-z]* ./ 2>/dev/null
-fi
-rm .project 2>/dev/null
-if [ -f ../.project ] || [ -f ../master/.project ]; then
-	cp ../.project ./ 2>/dev/null || cp ../master/.project ./ 2>/dev/null
-fi
-if [ -f .project ]; then
-	sed -re 's!@master</name>!@'"${NEW_TICKET}_${NEW_DESCR}"'</name>!' .project -i
-fi
+git pullme --force
+git fetch origin "${CURR_BRANCH}"
+git worktree add "../${NEW_DIR}" -b "${NEW_BRANCH}"
+( cd "../${NEW_DIR}" && git update && setup_new_worktree )
