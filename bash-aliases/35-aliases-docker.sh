9f7137fe622fc384c98fd5b2a626a44f7be03bbc
Oliver Hulett <ohulett@atlassian.com>
2019-04-10 12:00:02 +1000

    Autocommit from C02XV09UJGH7: 1 files changed (2019-04-10 12:00:02)
     M bash-aliases/35-aliases-docker.sh

--- bash-aliases/35-aliases-docker.sh
+++ bash-aliases/35-aliases-docker.sh
@@ -13,11 +13,11 @@ function _container_alias()
 
 ## Run docker-host; a container that dumps the network traffic it receives onto localhost.
 alias docker-host='_container_alias docker-host --cap-add=NET_ADMIN --cap-add=NET_RAW qoomon/docker-host'
-alias docker-postgres-ci='_container_alias docker-psql-ci -p 5433:5432 docker.atl-paas.net/jira-cloud/postgres-ci:9.5'
+alias docker-postgres-ci='_container_alias docker-psql-ci -p 5433:5432 -p 5432:5432 docker.atl-paas.net/jira-cloud/postgres-ci:9.5'
 ## Run postgres in a docker container.
 function docker-postgres()
 {
 	TAG="${1:-latest}"
-	_container_alias "docker-postgres" -p 5433:5432 "postgres:${TAG}"
+	_container_alias "docker-postgres" -p 5433:5432 -p 5432:5432 "postgres:${TAG}"
 }
 alias docker-clock='_container_alias docker-clock --privileged alpine hwclock -s'
