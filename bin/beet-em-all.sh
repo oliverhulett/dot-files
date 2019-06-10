359e043bb7cfb4a295d8d728c71632a51e1a9cbf
Oliver Hulett <ohulett@atlassian.com>
2019-06-10 19:00:05 +1000

    Autocommit from C02XV09UJGH7: 1 files changed (2019-06-10 19:00:05)
     M bin/beet-em-all.sh

--- bin/beet-em-all.sh
+++ bin/beet-em-all.sh
@@ -3,6 +3,48 @@
 set -x
 set -e
 
-for c in fingerprint replaygain acousticbrainz mbsync lyrics absubmit fetchart submit update move; do
+function print_help()
+{
+	echo "$(basename -- "$0") [-h|-?|--help]"
+	echo "$(basename -- "$0") [-a] [--all]"
+	echo "    -a --all: Run all the subcommands"
+}
+
+OPTS=$(getopt -o "a" --long "all" -n "$(basename -- "$0")" -- "$@")
+es=$?
+if [ $es != 0 ]; then
+	print_help
+	exit $es
+fi
+
+ALL="false"
+eval set -- "${OPTS}"
+while true; do
+	case "$1" in
+		-h | '-?' | --help )
+			print_help;
+			exit 0;
+			;;
+		-a | --all )
+			ALL="true"
+			shift
+			;;
+		-- ) shift; break ;;
+		* )
+			print_help
+			exit 1
+			;;
+	esac
+done
+
+CMDS=( "fingerprint" "replaygain" "acousticbrainz" "mbsync" "absubmit" "submit" "update" "move" )
+CMDS_EXTRA=( "lyrics" "fetchart" )
+for c in "${CMDS[@]}"; do
 	beet "$c"
 done
+if [ "${ALL}" == "true" ]; then
+	for c in "${CMDS_EXTRA[@]}"; do
+		beet "$c" &
+	done
+	wait -f
+fi
